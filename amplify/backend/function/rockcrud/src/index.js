3870e1adab59558435ceff36b34b882dfb2872bf
joaosass <j_henrique95@outlook.com>
2024-01-14 17:48:08 -0300

    feat: implement lambda crud

---
+++ amplify/backend/function/rockcrud/src/index.js
@@ -0,0 +1,98 @@
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
+const AWS = require('aws-sdk');
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const TableName = 'rocks-dev';
+
+const list = async () => {
+  const result = await dynamoDB.scan({TableName}).promise();
+  return result.Items;
+};
+
+const create = async Item => {
+  const params = {
+    TableName,
+    Item,
+  };
+
+  try {
+    await dynamoDB.put(params).promise();
+    return {message: 'Pedra criada com sucesso.'};
+  } catch (error) {
+    return {error: 'Erro ao criar pedra!'};
+  }
+};
+
+const update = async ({id, name, price}) => {
+  const params = {
+    TableName,
+    Key: {id},
+    UpdateExpression: 'SET #a = :x, #b = :y',
+    ExpressionAttributeNames: {
+      '#a': 'name',
+      '#b': 'price',
+    },
+    ExpressionAttributeValues: {
+      ':x': name,
+      ':y': price,
+    },
+  };
+
+  try {
+    await dynamoDB.update(params).promise();
+    return {message: 'Pedra atualizada com sucesso.'};
+  } catch (error) {
+    return {error: 'Erro ao atualizar pedra!'};
+  }
+};
+
+const remove = async Key => {
+  const params = {
+    TableName,
+    Key,
+  };
+
+  try {
+    await dynamoDB.delete(params).promise();
+    return {message: 'Pedra removida com sucesso.'};
+  } catch (error) {
+    return {error: 'Erro ao remover pedra!'};
+  }
+};
+
+exports.handler = async ({
+  body,
+  httpMethod,
+  queryStringParameters,
+  ...event
+}) => {
+  console.log(event);
+  let response;
+  switch (httpMethod) {
+    case 'GET':
+      response = await list();
+      break;
+    case 'POST':
+      response = await create(JSON.parse(body));
+      break;
+    case 'PUT':
+      response = await update(JSON.parse(body));
+      break;
+    case 'DELETE':
+      response = await remove(queryStringParameters);
+      break;
+    default:
+      response = 'Operação não suportada';
+  }
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': '*',
+    },
+    body: JSON.stringify(response),
+  };
+};
--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "rockapi": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
   "auth": {
     "rocktest908f096a": {
       "AppClientID": "string",
@@ -8,6 +15,25 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "rockcrud": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "rockdb": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "StreamArn": "string"
     }
   }
 }
