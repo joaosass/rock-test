3c0eea824f2f25b72cb931923fabe21d1cf45c85
joaosass <j_henrique95@outlook.com>
2024-01-16 18:40:00 -0300

    chore: install test dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   preset: 'react-native',
+  setupFilesAfterEnv: ['@testing-library/react-native/extend-expect'],
+  fakeTimers: {
+    enableGlobally: true,
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(@react-native|react-native|react-native-vector-icons)/)',
+  ],
 };
